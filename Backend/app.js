4f2192543fbddc7bb9579fa73af3f5a039134442
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-08-15 01:10:25 +0530

    allbooks(get)

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const mongoose = require('mongoose')
+const router = require("./routes/book-routes")
 
 const app = express();
 
 //Middleware
 
-app.use('/' , (req,res,next) => {
-    res.send("This is our Starting App")
-})
+app.use("/books",router)//localhost:5000/books
 
 mongoose.connect("mongodb+srv://bookapp:bookapp123@cluster0.cac3ogc.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log('Connected To Database'))
--- Backend/routes/book-routes.js
+++ Backend/routes/book-routes.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const Book = require("../model/Book");
 const router = express.Router();
 const Product = require("../model/Book")
 
+//This route provide all the books
+router.get("/" , async (req,res,next) => {
+    
+    let books;
+    try{
+        books = await Book.find();
+    } catch(err) {
+        console.log(err);
+    }
 
-router.get("/" , (req.res,next) => {
+    if(!books) {
+
+        return res.status(400).json({messege:"No Product Found"});
+    }
+
+       return res.status(200).json({books})
 
 } );
+
+module.exports = router;
